b857520059a45069d63c9f37c3b555df586c247e
Your Name <andrey.mglinec@gmail.com>
2021-07-01 23:58:34 +0300

    Add: add long press on backspace key

--- min.js
+++ min.js
@@ -2,15 +2,15 @@ let keys = document.querySelectorAll('.numbers_first_line');
 let output = document.getElementById('textBox');
 let backsp = document.querySelector('#backspace');
 
-
-
 for (let key of keys){
     key.onmousedown = () => {
         output.value += key.innerText;
     }
 }
-backsp.onmousedown = () =>{
+
+backsp.onmousedown = function back() {
     output.value = output.value.slice(0, - 1);
+    setTimeout(back, 100);
 }
 
 
