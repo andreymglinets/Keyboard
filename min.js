d00ed85f820b7ec386245f2464f3f792b64ca8ca
Your Name <andrey.mglinec@gmail.com>
2021-07-02 21:19:55 +0300

    Add: add code second-fourth line

--- min.js
+++ min.js
@@ -1,6 +1,10 @@
 let keys = document.querySelectorAll('.numbers_first_line');
+let liters = document.querySelectorAll('.liters_second_line');
 let output = document.getElementById('textBox');
 let backsp = document.querySelector('#backspace');
+let tab = document.querySelector('#tab');
+let thirdLiters = document.querySelectorAll('.liters_third_line');
+let fourtLiters = document.querySelectorAll('.liters_fourth_line');
 
 
 for (let key of keys){
@@ -8,6 +12,22 @@ for (let key of keys){
         output.value += key.innerText;
     }
 }
+for (let key of liters){
+    key.onmousedown = () => {
+        output.value += key.innerText;
+    }
+}
+for (let key of thirdLiters){
+    key.onmousedown = () => {
+        output.value += key.innerText;
+    }
+}
+for (let key of fourtLiters){
+    key.onmousedown = () => {
+        output.value += key.innerText;
+    }
+}
+
 function back(){
     output.value = output.value.slice(0, - 1);
 };
@@ -15,7 +35,7 @@ function back(){
 var timer;
 backsp.onmousedown = () => {
     output.value = output.value.slice(0, - 1);
-    timer = setInterval (back, 150);
+    timer = setInterval (back, 100);
 };
 
 backsp.onmouseup = () => {
@@ -29,5 +49,7 @@ space.onmousedown = () => {
     output.value += " ";
 }
 
-
+tab.onmousedown = () => {
+    output.value += '\t';
+}
 
