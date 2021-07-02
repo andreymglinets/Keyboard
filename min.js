8c24741981f703beedac4f7486b3da9c2056c45f
Your Name <andrey.mglinec@gmail.com>
2021-07-02 15:10:58 +0300

    Add: add log hold for backspace

--- min.js
+++ min.js
@@ -2,15 +2,31 @@ let keys = document.querySelectorAll('.numbers_first_line');
 let output = document.getElementById('textBox');
 let backsp = document.querySelector('#backspace');
 
+
 for (let key of keys){
     key.onmousedown = () => {
         output.value += key.innerText;
     }
 }
-
-backsp.onmousedown = function back() {
+function back(){
     output.value = output.value.slice(0, - 1);
-    setTimeout(back, 100);
+};
+
+var timer;
+backsp.onmousedown = () => {
+    output.value = output.value.slice(0, - 1);
+    timer = setInterval (back, 150);
+};
+
+backsp.onmouseup = () => {
+        clearInterval(timer);
+};
+
+
+let space = document.getElementById('space');
+
+space.onmousedown = () => {
+    output.value += " ";
 }
 
 
